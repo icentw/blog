50e098bbfaf0aee1ffd044f8acf0fde624d112ad
icentw <icemiyasawa@gmail.com>
2020-07-14 22:48:03 +0700

    Add firebase Edit navbar gitignore

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop > 1) {
+        $('#navbar').css('padding', '5px 25px')
+    } else {
+        $('#navbar').css('padding', '25px')
+    }
+})
