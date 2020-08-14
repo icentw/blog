593f34dea57d5fa69ad79ee6a79455a30b09ac90
icentw <icemiyasawa@gmail.com>
2020-08-14 22:15:06 +0700

    add blog-detail.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Section Navbar
 $(window).scroll(function() {
     var scrollTop = $(this).scrollTop();
     if (scrollTop > 1) {
@@ -6,3 +7,23 @@ $(window).scroll(function() {
         $('#navbar').css('padding', '25px')
     }
 })
+
+// Section owl-carousel
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
